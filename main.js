c213c166a5b500fb376f47a1907fc191cb80a7b1
Paul Hebert <paul.hebert@paulhebertdesigns.com>
2018-11-22 12:14:50 -0800

    Added planets. Playing with text overlays

--- main.js
+++ main.js
@@ -3,12 +3,10 @@ var options = {
 };
 
 var starPrototype = {
-    settings: {
-
-    },
+    className: 'star',
     size: {
-        max: 5,
-        min: 3
+        max: 4,
+        min: 2
     },
     colorRange: {
         red: {
@@ -26,13 +24,36 @@ var starPrototype = {
     }
 }
 
+var planetPrototype = {
+    className: 'planet',
+    size: {
+        max: 70,
+        min: 10
+    },
+    colorRange: {
+        red: {
+            min:100,
+            max:200
+        },
+        green: {
+            min:100,
+            max:200
+        },
+        blue: {
+            min:100,
+            max:200
+        }
+    }
+}
+
 var spaceField = {
     settings: {
         depth: 5,
         starDensity: 1/10000,
         starCount: function(layerDensity){
             return spaceField.size.area() * spaceField.settings.starDensity * layerDensity;
-        }
+        },
+        planetCount: 7
     },
     size: {
         width: null,
@@ -47,11 +68,11 @@ var spaceField = {
         spaceField.size.width = field.scrollWidth;
         spaceField.size.height = field.scrollHeight;
 
-        spaceField.functions.log('Field initiated.');
-        spaceField.functions.log('Settings:');
-        spaceField.functions.log(spaceField.settings, 'table');
-        spaceField.functions.log('Size:');
-        spaceField.functions.log(spaceField.size, 'table');
+        spaceField.functions.helpers.log('Field initiated.');
+        spaceField.functions.helpers.log('Settings:');
+        spaceField.functions.helpers.log(spaceField.settings, 'table');
+        spaceField.functions.helpers.log('Size:');
+        spaceField.functions.helpers.log(spaceField.size, 'table');
 
         for(var d = spaceField.settings.depth; d > 0; d--){
             var layer = document.createElement('div');
@@ -59,49 +80,91 @@ var spaceField = {
             field.append(layer);
 
             for(var i = 0; i < spaceField.settings.starCount(d); i++){
-                var star = document.createElement('div');
-                star.className = 'star';
+                spaceField.functions.generate.star(layer, d);
+            }
 
-                // get minimum size
-                var starSize = starPrototype.size.min;
-
-                // add a random number to get between min and max
-                starSize += (starPrototype.size.max - starPrototype.size.min) * Math.random();
-
-                // shrink stars in back layer.
-                starSize *= 1/d;
-
-                spaceField.functions.setStyles(star, {
-                    "left": Math.random() * spaceField.size.width,
-                    "top": Math.random() * spaceField.size.height,
-                    "width": starSize + "px",
-                    "height": starSize + "px",
-                    "background": spaceField.functions.randomRGB(starPrototype.colorRange),
-                    "box-shadow": `0px 0px ${1/d}px ${1/d}px rgba(255,255,255,0.33)`,
-                });
-
-                layer.append(star);
+            if(d === 1){
+                for(i = 0; i < spaceField.settings.planetCount; i++){
+                    spaceField.functions.generate.planet(layer);
+                }
             }
         }
     },
     functions: {
-        log: function(content, action = 'log'){
-            console[action](content);
-        },
-        setStyles: function(target, styles){
-            Object.keys(styles).forEach(function(key){
-                target.style[key] = styles[key];
-            });
-        },
-        randomRGB: function(colorRange){
-            var red = spaceField.functions.randomColorValue(colorRange.red)
-            var green = spaceField.functions.randomColorValue(colorRange.green)
-            var blue = spaceField.functions.randomColorValue(colorRange.blue)
+        generate: {
+            object: function(prototype, depth){
+                var object = document.createElement('div');
+                object.className = prototype.className;
 
-            return `rgb(${red},${green},${blue})`
+                objectSize = spaceField.functions.helpers.randomSize(prototype.size, depth);
+
+                spaceField.functions.helpers.setStyles(object, {
+                    "left": spaceField.functions.helpers.randomX(objectSize),
+                    "top": spaceField.functions.helpers.randomY(objectSize),
+                    "width": objectSize + "px",
+                    "height": objectSize + "px",
+                });
+
+                return object;
+            },
+            star: function(container, depth){
+                var star = spaceField.functions.generate.object(starPrototype, depth);
+
+                spaceField.functions.helpers.setStyles(star, {
+                    "background": spaceField.functions.helpers.randomRGB(starPrototype.colorRange),
+                    "box-shadow": `0px 0px ${1/depth}px ${1/depth}px rgba(255,255,255,0.33)`
+                });
+
+                container.append(star);
+            },
+            planet: function(container){
+                var planet = spaceField.functions.generate.object(planetPrototype, 1);
+
+                var backgroundHighlight = spaceField.functions.helpers.randomRGB(planetPrototype.colorRange)
+
+                spaceField.functions.helpers.setStyles(planet, {
+                    "background-image": `radial-gradient(#000 50%, ${backgroundHighlight})`,
+                    "box-shadow": "0px 0px 7px 3px rgba(255,255,255,0.1)"
+                });
+
+                container.append(planet);
+            }
         },
-        randomColorValue: function(color){
-            return color.min + ((color.max - color.min) * Math.random());
+        helpers: {
+            log: function(content, action = 'log'){
+                console[action](content);
+            },
+            setStyles: function(target, styles){
+                Object.keys(styles).forEach(function(key){
+                    target.style[key] = styles[key];
+                });
+            },
+            randomRGB: function(colorRange){
+                var red = spaceField.functions.helpers.randomColorValue(colorRange.red)
+                var green = spaceField.functions.helpers.randomColorValue(colorRange.green)
+                var blue = spaceField.functions.helpers.randomColorValue(colorRange.blue)
+
+                return `rgb(${red},${green},${blue})`
+            },
+            randomColorValue: function(color){
+                return color.min + ((color.max - color.min) * Math.random());
+            },
+            randomSize: function(sizeRange, depth){
+                // get minimum size
+                var size = sizeRange.min;
+
+                // add a random number to get between min and max
+                size += (sizeRange.max - sizeRange.min) * Math.random();
+
+                // shrink objects in back layers.
+                return size/depth;
+            },
+            randomX: function(diameter){
+                return (Math.random() * spaceField.size.width) - diameter/2;
+            },
+            randomY: function(diameter){
+                return (Math.random() * spaceField.size.height) - diameter/2;
+            }
         }
     }
 };
